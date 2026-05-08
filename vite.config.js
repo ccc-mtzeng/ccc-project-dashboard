65bda93ca43b162f0ca246625436ebdbb37be63e
Michael Tzeng <mtzeng@crosscountry-consulting.com>
2026-05-07 21:22:18 -0500

    vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Replace 'solution-tracker' with your GitHub repo name
-  base: "/solution-tracker/",
+  base: "/ccc-project-dashboard/",
 });
